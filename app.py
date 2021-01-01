26e87a7b878087014333d523f0c3d2d4f5cbc869
Govind155 <73738015+Govind155@users.noreply.github.com>
2021-01-01 18:34:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-import numpy as np
-from flask import Flask, request, render_template, url_for
+from flask import Flask, render_template, request
+import jsonify
+import requests
 import pickle
+import numpy as np
+import sklearn
 import joblib
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 model = pickle.load(open("students_mark_predictor.pkl", "rb"))
@@ -16,5 +20,6 @@ def predict():
     output = model.predict([value])[0][0].round(2)
     return render_template('index.html', Prediction_text = f"you will get {output}% marks, when you do study {input_features} hours per day")
 
-if __name__ == "__main__":
+
+if __name__=="__main__":
     app.run(debug=True)
