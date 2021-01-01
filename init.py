ddcd9caf7bfbd46bab607dc81e75f75b896d072e
Govind155 <73738015+Govind155@users.noreply.github.com>
2021-01-01 21:10:32 +0530

    Add files via upload

---
+++ init.py
@@ -0,0 +1,19 @@
+import numpy as np
+from flask import Flask, request, render_template, url_for
+import pickle
+import joblib
+
+app = Flask(__name__)
+model = joblib.load("Students_mark_predictor_model.pkl")
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    input_features = [float(x) for x in request.form.values()]
+    value = np.array(input_features)
+    output = model.predict([value])[0][0].round(2)
+    return render_template('index.html', Prediction_text = f"you will get {output}% marks, when you do study {input_features} hours per day")
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ version.py
@@ -0,0 +1,18 @@
+import flask
+import gunicorn
+import jinja2
+import markupsafe
+import werkzeug
+import numpy
+import scipy
+import sklearn
+import pandas 
+print(flask.__version__)
+print(gunicorn.__version__)
+print(jinja2.__version__)
+print(markupsafe.__version__)
+print(werkzeug.__version__)
+print(numpy.__version__)
+print(scipy.__version__)
+print(sklearn.__version__)
+print(pandas.__version__)
