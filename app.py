a24c23ab3a2987d80d5f224b00954fe2cb2f6472
Govind155 <73738015+Govind155@users.noreply.github.com>
2021-01-01 18:39:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,39 @@
-from flask import Flask, render_template, request
-import jsonify
-import requests
-import pickle
 import numpy as np
-import sklearn
+import pandas as pd
+from flask import Flask, request, render_template
 import joblib
-from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
-model = pickle.load(open("students_mark_predictor.pkl", "rb"))
-@app.route('/',methods=['GET'])
-def Home():
+
+model = joblib.load("student_mark_predictor.pkl")
+
+df = pd.DataFrame()
+
+@app.route('/')
+def home():
     return render_template('index.html')
 
-@app.route('/predict', methods = ['POST'])
+@app.route('/predict',methods=['POST'])
 def predict():
-    input_features = [float(x) for x in request.form.values()]
-    value = np.array(input_features)
-    output = model.predict([value])[0][0].round(2)
-    return render_template('index.html', Prediction_text = f"you will get {output}% marks, when you do study {input_features} hours per day")
+    global df
+    
+    input_features = [int(x) for x in request.form.values()]
+    features_value = np.array(input_features)
+    
+    #validate input hours
+    if input_features[0] <0 or input_features[0] >24:
+        return render_template('index.html', prediction_text='Please enter valid hours between 1 to 24 if you live on the Earth')
+        
+
+    output = model.predict([features_value])[0][0].round(2)
+
+    # input and predicted value store in df then save in csv file
+    df= pd.concat([df,pd.DataFrame({'Study Hours':input_features,'Predicted Output':[output]})],ignore_index=True)
+    print(df)   
+    df.to_csv('smp_data_from_app.csv')
+
+    return render_template('index.html', prediction_text='You will get [{}%] marks, when you do study [{}] hours per day '.format(output, int(features_value[0])))
+
 
 
 if __name__=="__main__":
