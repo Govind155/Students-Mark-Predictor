7516c2580883001c6f37d4ddd750847929df17bc
Govind155 <73738015+Govind155@users.noreply.github.com>
2021-01-01 18:48:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,26 @@
+from flask import Flask, render_template, request
+import jsonify
+import requests
+import pickle
 import numpy as np
-import pandas as pd
-from flask import Flask, request, render_template
+import sklearn
 import joblib
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
-
-model = joblib.load("student_mark_predictor.pkl")
-
-df = pd.DataFrame()
-
-@app.route('/')
-def home():
+model = pickle.load(open("student_mark_predictor.pkl", "rb"))
+@app.route('/',methods=['GET'])
+def Home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+
+standard_to = StandardScaler()
+@app.route("/predict", methods=['POST'])
 def predict():
     global df
     
+    if request.method == 'POST':
+
     input_features = [int(x) for x in request.form.values()]
     features_value = np.array(input_features)
     
@@ -27,14 +31,7 @@ def predict():
 
     output = model.predict([features_value])[0][0].round(2)
 
-    # input and predicted value store in df then save in csv file
-    df= pd.concat([df,pd.DataFrame({'Study Hours':input_features,'Predicted Output':[output]})],ignore_index=True)
-    print(df)   
-    df.to_csv('smp_data_from_app.csv')
-
     return render_template('index.html', prediction_text='You will get [{}%] marks, when you do study [{}] hours per day '.format(output, int(features_value[0])))
-
-
 
 if __name__=="__main__":
     app.run(debug=True)
